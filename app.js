e18a139d6b755b1c1f9a70a68319868c1af412bd
Diego Maeoka <diego.maeoka@infomentum.co.uk>
2021-09-16 12:44:08 +0100

    Rename env

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ console.log({
 	socketMode: true,
 	appToken: process.env.SLACK_APP_TOKEN,
 });
+
 const app = new App({
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
 	token: process.env.SLACK_BOT_TOKEN,
 	socketMode: true,
 	appToken: process.env.SLACK_APP_TOKEN,
